9abe437a8dfcc850d07f6b41bdaa7e5d6f658be4
Aldrich Pinso <78803960+stradrich@users.noreply.github.com>
2023-11-17 01:35:23 +0800

    attempt to deploy FE

--- src/pages/Navigation/LearnMore.vue
+++ src/pages/Navigation/LearnMore.vue
@@ -1,27 +1,62 @@
-  <template>
-    <div>
-      <Step v-for="(step, index) in steps" :key="index" :title="step.title">
-        {{ step.content }}
-      </Step>
-      <p>Ready to get started? Sign up today and embark on your hosting journey!</p>
+<script setup>
+import Navbar from '../../components/Navbar.vue';
+import Footer from '../../components/Footer.vue';
+</script>
+
+<template>
+  <Navbar/>
+  <div class="mt-10">
+    <div class="mx-5">
+      <h3>Step 1: Sign Up</h3>
+      <p>The first step to becoming a host is to sign up on our platform. It's a simple and quick process. Just provide some basic information about yourself and your space, and you'll be on your way.</p>
     </div>
-  </template>
-  
-  <script setup>
-  import Step from '../../components/Step'; // Update with the correct path
-  
-  const steps = [
-    { title: 'Step 1: Sign Up', content: 'The first step to becoming a host is to sign up on our platform. It\'s a simple and quick process. Just provide some basic information about yourself and your space, and you\'ll be on your way.' },
-    { title: 'Step 2: Create Your Listing', content: 'Once you\'re signed up, you can start creating your listing. Showcase your space with high-quality photos, write a compelling description, and set your availability and pricing. The more details you provide, the more likely you\'ll attract guests.' },
-    { title: 'Step 3: Set House Rules', content: 'You\'re in control of your space, so you get to set the rules. Specify any house rules, policies, or requirements that guests should be aware of. Clear expectations lead to better experiences for everyone.' },
-    { title: 'Step 4: Communicate with Guests', content: 'Guest communication is key to a successful hosting experience. Respond promptly to inquiries, provide helpful information, and make your guests feel welcome.' },
-    { title: 'Step 5: Prepare Your Space', content: 'Before guests arrive, ensure your space is clean, well-maintained, and ready for their stay. A clean and inviting space makes for happy guests.' },
-    { title: 'Step 6: Welcome Your Guests', content: 'When your guests arrive, give them a warm welcome. Provide them with any necessary information, keys, or access details they may need during their stay.' },
-    { title: 'Step 7: Be a Great Host', content: 'During your guest\'s stay, be available for any questions or assistance they may require. A great host can make a guest\'s experience unforgettable.' },
-    { title: 'Step 8: Collect Reviews', content: 'Encourage your guests to leave reviews after their stay. Positive reviews can help you attract more guests in the future.' },
-    { title: 'Step 9: Keep Improving', content: 'Hosting is a learning experience. Continuously improve your space and hosting skills based on guest feedback and your own observations.' },
-    { title: 'Step 10: Earn Rewards', content: 'As a host, you can earn rewards and build a reputation for your hospitality. Hosting can be a source of income and a way to meet interesting people from all over the world.' },
-    // Add other steps as needed
-  ];
-  </script>
-  
+
+    <div class="mx-5 mt-2">
+      <h3>Step 2: Create Your Listing</h3>
+      <p>Once you're signed up, you can start creating your listing. Showcase your space with high-quality photos, write a compelling description, and set your availability and pricing. The more details you provide, the more likely you'll attract guests.</p>
+    </div>
+
+    <div class="mx-5 mt-2">
+      <h3>Step 3: Set House Rules</h3>
+      <p>You're in control of your space, so you get to set the rules. Specify any house rules, policies, or requirements that guests should be aware of. Clear expectations lead to better experiences for everyone.</p>
+    </div>
+
+    <div class="mx-5 mt-2">
+      <h3>Step 4: Communicate with Guests</h3>
+      <p>Guest communication is key to a successful hosting experience. Respond promptly to inquiries, provide helpful information, and make your guests feel welcome.</p>
+    </div>
+
+    <div class="mx-5 mt-2">
+      <h3>Step 5: Prepare Your Space</h3>
+      <p>Before guests arrive, ensure your space is clean, well-maintained, and ready for their stay. A clean and inviting space makes for happy guests.</p>
+    </div>
+
+    <div class="mx-5 mt-2">
+      <h3>Step 6: Welcome Your Guests</h3>
+      <p>When your guests arrive, give them a warm welcome. Provide them with any necessary information, keys, or access details they may need during their stay.</p>
+    </div>
+
+    <div class="mx-5 mt-2">
+      <h3>Step 7: Be a Great Host</h3>
+      <p>During your guest's stay, be available for any questions or assistance they may require. A great host can make a guest's experience unforgettable.</p>
+    </div>
+
+    <div class="mx-5 mt-2">
+      <h3>Step 8: Collect Reviews</h3>
+      <p>Encourage your guests to leave reviews after their stay. Positive reviews can help you attract more guests in the future.</p>
+    </div>
+
+    <div class="mx-5 mt-2">
+      <h3>Step 9: Keep Improving</h3>
+      <p>Hosting is a learning experience. Continuously improve your space and hosting skills based on guest feedback and your own observations.</p>
+    </div>
+
+    <div class="mx-5 mt-2">
+      <h3>Step 10: Earn Rewards</h3>
+      <p>As a host, you can earn rewards and build a reputation for your hospitality. Hosting can be a source of income and a way to meet interesting people from all over the world.</p>
+    </div>
+
+    <p class="mx-5 mt-2">Ready to get started? Sign up today and embark on your hosting journey!</p>
+  </div>
+  <Footer/>
+</template>
